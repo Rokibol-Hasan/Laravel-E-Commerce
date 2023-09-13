32bd0eee5b358476eb4ea954afb59ded93576e41
Rokibol-Hasan <rokibul.techdynobd@gmail.com>
2023-09-13 21:31:10 +0600

    Frontend Mastering Complete

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('frontend.pages.home');
+});
+
+Route::get('/dashboard', function () {
     return view('backend.pages.dashboard');
 });
+
