67fcee3fe5ef1feab4702e98dd98b9895cd7cb7f
Rokibol-Hasan <rokibul.techdynobd@gmail.com>
2023-09-20 13:37:06 +0600

    vite config

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::get('/dashboard', function () {
     return view('backend.pages.dashboard');
 });
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
